5bd1bff1753a5d73bb8db4b2bf4f647522d2821b
Sadman Ryan Riad <87647544+sadmanryanriad@users.noreply.github.com>
2023-10-06 20:46:41 +0600

    background added

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,13 +1,15 @@
-import Navbar from '../components/Header/Navbar';
-import { Outlet } from 'react-router-dom';
+import Navbar from "../components/Header/Navbar";
+import { Outlet } from "react-router-dom";
 
 const Root = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div className="w-full h-screen" style={{backgroundImage: 'url(https://i.ibb.co/cX6Rm9b/scattered-forcefields.png)'}}>
+      <div className="max-w-6xl mx-auto">
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
 };
 
 export default Root;
